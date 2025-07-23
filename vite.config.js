8acdc978c9ad51a07003b75febdf009449be5ed1
BeteranoCode <219803109+BeteranoCode@users.noreply.github.com>
2025-07-23 17:31:20 +0200

    Corrige estructura: mueve index.html a raíz y elimina input incorrecto de Vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/beterano-map/',
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'public/index.html'
-    }
-  }
+  plugins: [react()]
 });
