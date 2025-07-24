667b3e50d06e65e675c168f6f83cc118f74e806d
BeteranoCode <219803109+BeteranoCode@users.noreply.github.com>
2025-07-24 22:48:40 +0200

    🎨 Añadidos estilos base SCSS desde Precious Plastic y cargados en main.jsx

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/beterano-map/',
-  plugins: [react()]
+  plugins: [react()],
+    css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/sass/index.scss";`
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
