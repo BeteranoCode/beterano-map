81e4e42ffc27126646fe2789e8a232465df4dca5
BeteranoCode <219803109+BeteranoCode@users.noreply.github.com>
2025-08-04 17:05:17 +0200

    🔧 Fix rutas absolutas para GitHub Pages y ajusta vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -24,7 +23,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      input: 'index.html'
+      input: path.resolve(__dirname, 'index.html')
     }
   }
 });
